6868bcfb1729e69af1f5f40dbb428bf2b48f1815
Edil Medeiros <j.edil@ene.unb.br>
2022-12-06 10:55:48 -0300

    Solve Puzzle 2

--- src/AoC/Day01/Puzzle01.hs
+++ src/AoC/Day01/Puzzle01.hs
@@ -1,5 +1,6 @@
-
 module AoC.Day01.Puzzle01 where
+
+import Data.List
 
 test :: String
 test = "123\n234\n345\n\n456\n\n567"
@@ -10,11 +11,11 @@ testLines = lines test
 file :: FilePath
 file = "src/AoC/Day01/data.txt"
 
-answer :: IO Int
-answer = maxCalories 0 . lines <$> readFile file
+answer1 :: IO Int
+answer1 = maxCalories 0 . lines <$> readFile file
 
-answer' :: IO Int
-answer' = maxCalories' 0 . lines <$> readFile file
+answer1' :: IO Int
+answer1' = maxCalories' 0 . lines <$> readFile file
 
 
 -- First version
@@ -46,5 +47,12 @@ groupElfs [] = []
 groupElfs ss = actual : groupElfs (drop 1 next)
   where (actual, next) = span (not . null) ss
 
-answer'' :: IO Int
-answer'' = maximum . fmap elfCalories . groupElfs . lines <$> readFile file
+answer1'' :: IO Int
+answer1'' = maximum . fmap elfCalories . groupElfs . lines <$> readFile file
+
+
+-- The second part asks the calories of the top three elves. Just add
+-- a few more things to the pipeline
+
+answer2 :: IO Int
+answer2 = sum . take 3 . sortBy (flip compare) . fmap elfCalories . groupElfs . lines <$> readFile file
